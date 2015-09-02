f941db0dc789105b43f73271a5030d25090ba489
John <John@10.30.20.171>
2015-09-02 17:51:15 -0500

    minor tweaks

---
+++ src/abstractionassignment/CuttingDevice.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractionassignment;
+
+/**
+ *
+ * @author John
+ */
+public class CuttingDevice extends HandheldTool{
+    
+}
